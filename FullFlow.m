105103f1e3eddeda999aea3cc7cee2d468b963af
Ilias Ibnyahya <35698838+ilias-audio@users.noreply.github.com>
2022-10-27 18:14:03 +0100

    First public release

--- FullFlow.m
+++ FullFlow.m
@@ -1,15 +1,21 @@
+%% First Steps
 % run the python script 
 % add functions and libs to path
 
-%% setup Steps
+%% Setup Steps
 current_dir = pwd;
 target_IR_folder = fullfile(pwd, "IR_mono");
 target_mesures_dir = fullfile(pwd, "results/target/measures");
 MeasureImpulseResponsesDirectory(target_IR_folder, target_mesures_dir);
 %% Optimization Steps
-FDN_Only_results = fullfile(pwd, "results/FDN");
+FDN_Only_results = fullfile(pwd, "results/generated");
 MatchReverbDirectory(target_mesures_dir, FDN_Only_results, "FDN_Only");
 
 Hybrid_results = fullfile(pwd, "results/hybrid");
 MatchReverbDirectory(target_mesures_dir, Hybrid_results, "Hybrid");
-%% Evaluation Steps
+%% FDN_only Evaluation Steps
+Gen_measures_folder = fullfile(FDN_Only_results, "measures");
+CompareMeasuresDirectories(target_mesures_dir, Gen_measures_folder);
+%% Hybrid Evaluation Steps
+Hyb_measures_folder = fullfile(Hybrid_results, "measures");
+CompareMeasuresDirectories(target_mesures_dir, Hyb_measures_folder);
